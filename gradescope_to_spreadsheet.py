937a5398f040d0c8c5f762e2c23cabd62a07f9ad
Naveen Sg Nathan <naveen.nathan@berkeley.edu>
2024-07-29 19:54:18 -0700

    Pushes grades to sheets with proper formatting

--- gradescope_to_spreadsheet.py
+++ gradescope_to_spreadsheet.py
@@ -14,7 +14,6 @@ ASSIGNMENT_ID = "4486584"
 # This scope allows for write access.
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 SPREADSHEET_ID = "1yrHEpO5dOMutG6mfDRwauxqT6F0nirqFAEdqMn8kRhU"
-SAMPLE_RANGE_NAME = "Class Data!A2:E"
 
 def allow_user_to_authenticate_google_account():
   """
@@ -42,7 +41,7 @@ def allow_user_to_authenticate_google_account():
       token.write(creds.to_json())
   return creds
 
-def readFromSheet(creds):
+def writeToSheet(creds, assignment_scores):
   try:
     service = build("sheets", "v4", credentials=creds)
     
@@ -52,12 +51,12 @@ def readFromSheet(creds):
     sheet = service.spreadsheets()
     sheets = sheet.get(spreadsheetId=SPREADSHEET_ID, fields='sheets/properties/title').execute()
     sub_sheet_titles = [sheet['properties']['title'] for sheet in sheets['sheets']]
+    # Need to keep track of subsheet ids as well
 
     print(sub_sheet_titles)
 
     if ASSIGNMENT_ID not in sub_sheet_titles:
       #create new sub_sheet
-      pass
       try:
          sheetservice = build('sheets', 'v4', credentials=creds)
         
@@ -70,8 +69,30 @@ def readFromSheet(creds):
                 }
             }
          }
-
          sheetservice.spreadsheets().batchUpdate(spreadsheetId=SPREADSHEET_ID, body=body).execute()
+         #f = open("assignment_scores.csv", "r")
+         #file_contents = f.read()
+         assignment_scores = assignment_scores.replace("\\n", "\n")
+         body = {
+            'requests': [
+                {
+                'pasteData': {
+                    "coordinate": {
+                        "sheetId": "0",
+                        "rowIndex": "0",  
+                        "columnIndex": "0",
+                    },
+                    "data": str(assignment_scores),
+                    "type": 'PASTE_NORMAL',
+                    "delimiter": ',',
+                }
+            }
+            ]
+        }
+         sheetservice.spreadsheets().batchUpdate(spreadsheetId=SPREADSHEET_ID, body=body).execute()
+    
+
+
         
       except HttpError as err:
         print(err)     
@@ -83,10 +104,10 @@ def readFromSheet(creds):
     print(err)
 
 def main():
-    #gradescope_client =  client.GradescopeClient()
-    #gradescope_client.prompt_login()
-    #assignment_scores = gradescope_client.download_scores(COURSE_ID, ASSIGNMENT_ID)
+    gradescope_client =  client.GradescopeClient()
+    gradescope_client.prompt_login()
+    assignment_scores = str(gradescope_client.download_scores(COURSE_ID, ASSIGNMENT_ID))
     creds = allow_user_to_authenticate_google_account()
-    readFromSheet(creds)
+    writeToSheet(creds, assignment_scores)
 
 main()
