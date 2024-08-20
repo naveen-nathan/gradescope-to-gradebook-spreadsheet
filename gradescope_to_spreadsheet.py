970b4c35346348c27d5fd217afa79e9760f10aad
Naveen Sg Nathan <naveen.nathan@berkeley.edu>
2024-08-19 22:42:25 -0700

    Added logic to push one assignment's grades to dashboard

--- gradescope_to_spreadsheet.py
+++ gradescope_to_spreadsheet.py
@@ -4,6 +4,8 @@ from fullGSapi.api import client
 import os.path
 import sys
 import re
+import pandas as pd
+import io
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -17,7 +19,8 @@ COURSE_ID = "782967"
 ASSIGNMENT_ID = (len(sys.argv) > 1) and sys.argv[1]
 # This scope allows for write access.
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
-SPREADSHEET_ID = "1yrHEpO5dOMutG6mfDRwauxqT6F0nirqFAEdqMn8kRhU"
+SPREADSHEET_ID = "1kaC1SyYCbQKyZByxu_sX96D-bdGAlMrFWqLXKrivcxU" #"1yrHEpO5dOMutG6mfDRwauxqT6F0nirqFAEdqMn8kRhU"
+NUMBER_OF_STUDENTS = 77
 
 """
 Allows the user authenticate their google account, allowing the script to modify spreadsheets in their name.
@@ -51,13 +54,9 @@ def allow_user_to_authenticate_google_account():
 def writeToSheet(creds, assignment_scores):
     try:
 
-        service = build("sheets", "v4", credentials=creds)
-        sheet_api_instance = service.spreadsheets()
+        sheet_api_instance = create_sheet_api_instance(creds)
 
-        sheets = sheet_api_instance.get(spreadsheetId=SPREADSHEET_ID, fields='sheets/properties').execute()
-
-        sub_sheet_titles_to_ids = {sheet['properties']['title']: sheet['properties']['sheetId'] for sheet in
-                                   sheets['sheets']}
+        sub_sheet_titles_to_ids = get_sub_sheet_titles_to_ids(sheet_api_instance)
 
         sheet_id = None
 
@@ -76,26 +75,43 @@ def writeToSheet(creds, assignment_scores):
         else:
             sheet_id = sub_sheet_titles_to_ids[ASSIGNMENT_ID]
 
-        push_grade_data_request = {
-            'requests': [
-                {
-                    'pasteData': {
-                        "coordinate": {
-                            "sheetId": sheet_id,
-                            "rowIndex": 0,
-                            "columnIndex": 0,
-                        },
-                        "data": assignment_scores,
-                        "type": 'PASTE_NORMAL',
-                        "delimiter": ',',
-                    }
-                }
-            ]
-        }
-        sheet_api_instance.batchUpdate(spreadsheetId=SPREADSHEET_ID, body=push_grade_data_request).execute()
+        update_sheet_with_csv(assignment_scores, sheet_api_instance, sheet_id)
         print("Successfully updated spreadsheet with new score data")
     except HttpError as err:
         print(err)
+
+
+def create_sheet_api_instance(creds):
+    service = build("sheets", "v4", credentials=creds)
+    sheet_api_instance = service.spreadsheets()
+    return sheet_api_instance
+
+
+def get_sub_sheet_titles_to_ids(sheet_api_instance):
+    sheets = sheet_api_instance.get(spreadsheetId=SPREADSHEET_ID, fields='sheets/properties').execute()
+    sub_sheet_titles_to_ids = {sheet['properties']['title']: sheet['properties']['sheetId'] for sheet in
+                               sheets['sheets']}
+    return sub_sheet_titles_to_ids
+
+
+def update_sheet_with_csv(assignment_scores, sheet_api_instance, sheet_id, rowIndex = 0, columnIndex=0):
+    push_grade_data_request = {
+        'requests': [
+            {
+                'pasteData': {
+                    "coordinate": {
+                        "sheetId": sheet_id,
+                        "rowIndex": rowIndex,
+                        "columnIndex": columnIndex,
+                    },
+                    "data": assignment_scores,
+                    "type": 'PASTE_NORMAL',
+                    "delimiter": ',',
+                }
+            }
+        ]
+    }
+    sheet_api_instance.batchUpdate(spreadsheetId=SPREADSHEET_ID, body=push_grade_data_request).execute()
 
 
 def retrieve_grades_from_gradescope():
@@ -121,9 +137,8 @@ def get_assignment_info(gs_instance, class_id: str) -> bytes:
     return res.content
 
 
-def make_score_sheet_for_one_assignment():
+def make_score_sheet_for_one_assignment(creds):
     assignment_scores = retrieve_grades_from_gradescope()
-    creds = allow_user_to_authenticate_google_account()
     writeToSheet(creds, assignment_scores)
 
 """
@@ -134,10 +149,37 @@ def get_assignment_id_to_names():
     course_info_response = str(get_assignment_info(initialize_gs_client(), COURSE_ID)).replace("\\\\", "\\")
     pattern = '{"id":[0-9]+,"title":"[\w,:\+\s()]+?"}'
     info_for_all_assignments = re.findall(pattern, course_info_response)
-    assignment_to_names = { json.loads(assignment)['id'] : json.loads(assignment)['title'] for assignment in info_for_all_assignments }
+    assignment_to_names = { str(json.loads(assignment)['id']) : json.loads(assignment)['title'] for assignment in info_for_all_assignments }
     return assignment_to_names
 
 def main():
-    make_score_sheet_for_one_assignment()
+    creds = allow_user_to_authenticate_google_account()
+    make_score_sheet_for_one_assignment(creds)
 
-main()
+def populate_instructor_dashboard():
+    assignment_id_to_names = get_assignment_id_to_names()
+    sorted_keys = sorted(assignment_id_to_names.keys())  # sort based on labs and discussions separately
+    labs = filter(lambda assignment: "lab" in assignment.lower(),
+                  assignment_id_to_names.values())
+    sorted_labs = sorted(labs, key=lambda lab: int(re.findall("\d+", lab)[0]))
+    #lab_ids_to_ids = {lab_name : lab_id for lab_name, lab_id in sorted_labs.items()}
+    creds = allow_user_to_authenticate_google_account()
+    sheet_api_instance = create_sheet_api_instance(creds)
+    sub_sheet_titles_to_ids = get_sub_sheet_titles_to_ids(sheet_api_instance)
+    dashboard_sheet_id = sub_sheet_titles_to_ids['Instructor_Dashboard']
+    assignment_id = ASSIGNMENT_ID
+
+    assignment_name = assignment_id_to_names[assignment_id]
+    spreadsheet_query = f"=DIVIDE(XLOOKUP(A:A, {assignment_id}!A:A, {assignment_id}!E:E), XLOOKUP(A:A, {assignment_id}!A:A, {assignment_id}!F:F))"
+    dashboard_column = {assignment_name : [spreadsheet_query] * NUMBER_OF_STUDENTS}
+    dashboard_df = pd.DataFrame(dashboard_column).set_index(assignment_name)
+    output = io.StringIO()
+    dashboard_df.to_csv(output)
+    #print(output.getvalue())
+    update_sheet_with_csv(output.getvalue(), sheet_api_instance, dashboard_sheet_id, 0, 5)
+    output.close()
+
+
+
+populate_instructor_dashboard()
+
